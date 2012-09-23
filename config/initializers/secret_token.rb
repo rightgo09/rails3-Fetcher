28c3af30186f4129ac73cdf5f265156419d0d9cd
rightgo09 <skyarrow09@gmail.com>
2012-09-24 08:53:00 +0900

    `rails new Fetcher -T`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fetcher::Application.config.secret_token = '4f70ad76150f4d0f461e9ffa851250e63e7383b70a5303002520fb7ccc74b2d7eaea014a09624031efaf717859569217fa0c6742af8bc4cd3613e56d68eb8ca7'
